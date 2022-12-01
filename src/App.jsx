5de30922f4b00e919b710ed7984b30b719cb35c6
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-01 14:26:18 -0300

    Importacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+// Components
+import BottomFooter from "./components/BottomFooter";
+import TopFooter from "./components/TopFooter";
+import Beginning from "./components/Beginning";
+
+// Styles
+import "./styles/reset.css";
+import "./scss/styles.scss";
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Beginning />
+      <TopFooter />
+      <BottomFooter />
+    </>
   )
 }
 
