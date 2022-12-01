349b603739f22f0da0c3336a81ce51f8676fcaf0
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-01 14:25:53 -0300

    Parte inicial de la página

--- src/components/Beginning.jsx
+++ src/components/Beginning.jsx
@@ -1,8 +1,13 @@
+// Components
+import Navbar from "./Navbar";
+
 // Styles
 import "../scss/beginning.scss";
 
 const Beginning = () => {
-  return <div className="beginning"></div>;
+  return <div className="beginning">
+    <Navbar />
+  </div>;
 };
 
 export default Beginning;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,45 @@
+// React Hooks
+import { useState } from "react";
+
+// React Icons
+import { FiMenu } from "react-icons/fi";
+import { ImCross } from "react-icons/im";
+
+// Styles
+import "../scss/navbar.scss";
+
 const Navbar = () => {
+  const [active, setActive] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <img className="navbar__logo" src={"/images/logonav.svg"}/>
+      <div className={`navbar__links ${active && "active"}`}>
+        <ul>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">Community</a>
+          </li>
+        </ul>
+      </div>
+      <button className="navbar__button">Get Started</button>
+      {
+        !active ? <FiMenu className="toggle-button" onClick={() => setActive(!active)} /> :
+        <ImCross className="toggle-button" onClick={() => setActive(!active)} />
+      }
+      
+
+    </nav>
   )
 }
 
