48dc7c442669541afb9983d5679fc1ef88efc00c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-02 22:09:23 -0300

    Datos para las opiniones

---
+++ src/util/opinions.js
@@ -0,0 +1,34 @@
+// Images
+import ali from "../img/avatar-ali.png"
+import richard from "../img/avatar-richard.png";
+import shanai from "../img/avatar-shanai.png";
+import anisha from "../img/avatar-anisha.png";
+
+const opinions = [
+  {
+    img: ali,
+    name: "Ali Bravo",
+    opinion:
+      "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."
+  },
+  {
+    img: richard,
+    name: "Richard Watts",
+    opinion:
+      "Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to!"
+  },
+  {
+    img: shanai,
+    name: "Shanai Gough",
+    opinion:
+      "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync being intrusive."
+  },
+  {
+    img: anisha,
+    name: "Anisha Li",
+    opinion:
+      "Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."
+  },
+];
+
+export default opinions;
