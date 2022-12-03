ce0a92a8478de19d973752687de2f1e5a8504024
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-02 22:12:07 -0300

    Importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 // Components
-import BottomFooter from "./components/BottomFooter";
-import TopFooter from "./components/TopFooter";
 import Beginning from "./components/Beginning";
+import Features from "./components/Features";
+import Opinions from "./components/Opinions";
+import TopFooter from "./components/TopFooter";
+import BottomFooter from "./components/BottomFooter";
 
 // Styles
 import "./styles/reset.css";
@@ -11,6 +13,8 @@ const App = () => {
   return (
     <>
       <Beginning />
+      <Features />
+      <Opinions />
       <TopFooter />
       <BottomFooter />
     </>
